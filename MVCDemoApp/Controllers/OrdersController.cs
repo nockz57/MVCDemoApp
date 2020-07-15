a6db11c31bd63c28e0791282cf5c96ca2a40f522
Manuel Phetla <mnockz@gmail.com>
2020-07-15 20:44:43 +0200

    First comit

---
+++ MVCDemoApp/Controllers/OrdersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVCDemoApp.Models;
+
+namespace MVCDemoApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IFoodData _foodData;
+        private readonly IOrderData _orderData;
+
+        public OrdersController(IFoodData foodData, IOrderData orderData)
+        {
+            _foodData = foodData;
+            _orderData = orderData;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var food = await _foodData.GetFood();
+            CreateOrderModel model = new CreateOrderModel();
+
+            food.ForEach(x =>
+            {
+                model.FoodItems.Add(new SelectListItem { Value = x.Id.ToString(), Text = x.Title });
+            });
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderModel order)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View();
+            }
+
+            var food = await _foodData.GetFood();
+            order.Total = order.Quantity * food.Where(x => x.Id == order.FoodId).First().Price;
+
+            int id = await _orderData.CreateOrder(order);
+
+            return RedirectToAction("Display", new { id});
+        }
+
+        public async Task<IActionResult> Display(int id)
+        {
+            OrderDisplayModel displayOrder = new OrderDisplayModel();
+            displayOrder.Order = await _orderData.GetOrderById(id);
+
+            if (displayOrder.Order != null)
+            {
+                var food = await _foodData.GetFood();
+                displayOrder.ItemPurchased = food.Where(x => x.Id == displayOrder.Order.FoodId).FirstOrDefault()?.Title;
+            }
+
+            return View(displayOrder);
+        }
+    }
+}
